ceced12a4a7062f18fccc619967e0a724207a74b
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-13 11:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 //your code here
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '').trim();
+}
+ 
+// Array of band names
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+ 
+// Remove articles and sort the band names
+let sortedBandNames = bandNames.map(removeArticles).sort();
+ 
+// Create an HTML list
+const ul = document.createElement('ul');
+ul.id = 'band';
+ 
+sortedBandNames.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
+ 
+// Append the list to the document's body or another desired location
+document.body.appendChild(ul);
 // Define the array of band names
 let bandNames = ['The Rolling Stones', 'Aerosmith', 'Metallica', 'The Beatles', 'Led Zeppelin', 'Nirvana'];
 
